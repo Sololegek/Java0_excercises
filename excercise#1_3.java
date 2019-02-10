5a351a5e5f559aa2876cc72adb41ec2a69ee2be5
Aleh Salavei <47496416+Sololegek@users.noreply.github.com>
2019-02-10 22:31:04 +0300

    Add files via upload

---
+++ excercise#1_3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Math.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print("Введите радиусы: R1 = ");
+            float r1 = Float.parseFloat(in.readLine());
+            System.out.print("R2 = ");
+            float r2 = Float.parseFloat(in.readLine());
+
+            if (r1 > r2) {
+                double result = PI * pow(r1, 2) - PI * pow(r2, 2);
+                System.out.println("Площадь кольца составляет: " + result);
+                break;
+            } else System.out.println("Неправильный ввод! R1 должен быть больше R2!");
+
+        }
+    }
+}
