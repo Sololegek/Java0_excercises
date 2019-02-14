91514ae897ea94297bc623ea2f0c98d4cca41c97
Aleh Salavei <47496416+Sololegek@users.noreply.github.com>
2019-02-14 16:13:34 +0300

    Update exercise#1_5.java

--- exercise#1_5.java
+++ exercise#1_5.java
@@ -8,19 +8,21 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите целое шестизначное число: ");
         int num = Integer.parseInt(in.readLine());
-        int[] digits = new int[6];
+        int digit;
         float ar_aver = 0;
         double g_aver = 1;
+        int count = 0;
 
-        for (int i = 0; i < digits.length; i++) {
-            digits[i] = num % 10;
+        do {
+            digit = num % 10;
             num = num / 10;
-            ar_aver = ar_aver + digits[i];
-            g_aver = g_aver * digits[i];
-        }
+            ar_aver = ar_aver + digit;
+            g_aver = g_aver * digit;
+            count++;
+        } while (num > 0);
 
-        ar_aver = ar_aver / digits.length;
-        g_aver = Math.pow(g_aver, 1.0/digits.length);
+        ar_aver = ar_aver / count;
+        g_aver = Math.pow(g_aver, 1.0/count);
 
         System.out.println("Среднее арифметическое: " + String.format("%.3f", ar_aver));
         System.out.println("Среднее геометрическое: " + String.format("%.3f", g_aver));
