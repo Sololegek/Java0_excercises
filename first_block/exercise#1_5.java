a9e4a0db1fc9d2b18a2adada74d524cdd03951d5
Aleh Salavei <47496416+Sololegek@users.noreply.github.com>
2019-02-20 16:23:35 +0300

    Update exercise#1_5.java

--- first_block/exercise#1_5.java
+++ first_block/exercise#1_5.java
@@ -6,23 +6,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите целое шестизначное число: ");
-        int num = Integer.parseInt(in.readLine());
+        int num;
         int digit;
         float ar_aver = 0;
         double g_aver = 1;
-        int count = 0;
+
+        do {
+            System.out.println("Введите целое шестизначное число: ");
+            num = Integer.parseInt(in.readLine());
+        } while (num < 99999 || num > 1000000);
 
         do {
             digit = num % 10;
             num = num / 10;
             ar_aver = ar_aver + digit;
             g_aver = g_aver * digit;
-            count++;
         } while (num > 0);
 
-        ar_aver = ar_aver / count;
-        g_aver = Math.pow(g_aver, 1.0/count);
+        ar_aver = ar_aver / 6.0f;
+        g_aver = Math.pow(g_aver, 1.0/6.0);
 
         System.out.println("Среднее арифметическое: " + String.format("%.3f", ar_aver));
         System.out.println("Среднее геометрическое: " + String.format("%.3f", g_aver));
