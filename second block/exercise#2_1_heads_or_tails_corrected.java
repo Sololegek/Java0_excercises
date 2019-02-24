0d5510659a6644ceda33e8079de1d4e7ffccb53b
Aleh Salavei <47496416+Sololegek@users.noreply.github.com>
2019-02-24 23:29:07 +0300

    Исправленные версии

---
+++ second block/exercise#2_1_heads_or_tails_corrected.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        Random r = new Random();
+        int heads = 0;
+        int tails = 0;
+
+        for (int i = 0; i < 1000; i++){
+            if (r.nextBoolean()){
+                heads++;
+            } else{
+                tails++;
+            }
+        }
+
+        System.out.println("Орёл выпал "+ heads + " раз, а решка - " + tails + ".");
+    }
+}
---
+++ second block/exercise#2_4_friendly_nums_corrected.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Random r = new Random();
+        int num = Math.abs(r.nextInt(10000));
+        int num2 = Math.abs(r.nextInt(100000));
+        int count = 0;
+        int tempNum = num;
+
+        num = num == 0 ? num + 1 : num;
+        num2 = num2 == 0 ? num2 + 1 : num2;
+
+        if (num > num2){
+            tempNum = num2;
+            num2 = num;
+            num = tempNum;
+        }
+        System.out.println("Нижний предел: " + num);
+        System.out.println("Верхний предел: " + num2);
+
+        while (tempNum < num2){
+            int result = findSum(tempNum);
+            if (result < num2 & result > num) {
+                int result2 = findSum(result);
+                if (result2 == tempNum & tempNum != result) {
+                    System.out.println("Числа " + tempNum + " и " + result + " являются дружественными.");
+                    count++;
+                }
+            }
+            tempNum++;
+        }
+
+        if (count == 0){
+            System.out.println("В диапазоне от " + num + " до " + num2 + " дружественных чисел не найдено.");
+        } else {
+            System.out.println("В диапазоне от " + num + " до " + num2 + " найдено " + count + " дружественных чисел.");
+        }
+
+    }
+
+    private static int findSum(int num){
+        int result = 0;
+        for (int divider = 1; divider <= num / 2; divider++){
+            if (num % divider == 0){
+                result += divider;
+            }
+        }
+        return result;
+    }
+}
